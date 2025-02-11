08ce40b0216b8dfb18066c1cd17564d13a714190
J5F7BUK231 <velyacsgo@gmail.com>
2025-02-11 14:01:55 +0300

    fix spin pitch

--- test_lua.lua
+++ test_lua.lua
@@ -1305,7 +1305,7 @@ local Vars = {
 			defensive_aa = group:checkbox('Manuals  »  Defensive AA\nmanuals'),
 			defensive_pitch = group:combobox('Manuals  »  Pitch\ndefensive_pitch\nmanuals', {'Disabled', 'Up', 'Zero', 'Random', 'Spin', 'Custom'}),
 			pitch_slider = group:slider('\ncustom_defensive_pitch\nmanuals', -89, 89, 0),
-			defensive_yaw = group:combobox('Manuals  »  Yaw\ndefensive_yaw\nmanuals', {'Disabled', 'Sideways', 'Opposite', "Spin", "Leg Breaker",  'Flick', 'Switch', 'Spin180', 'Jitter90', 'RandomYaw', 'RandomStaticYaw', 'Spin', 'Custom'}),
+			defensive_yaw = group:combobox('Manuals  »  Yaw\ndefensive_yaw\nmanuals', {'Disabled', 'Sideways', 'Opposite', "Spin", "Leg Breaker",  'Flick', 'Switch', 'Spin180', 'Jitter90', 'RandomYaw', 'RandomStaticYaw', 'Custom'}),
 			yaw_slider = group:slider('\ncustom_defensive_yaw\nmanuals', -180, 180, 0)
 		
 		},
@@ -3294,7 +3294,7 @@ conditional_antiaims.handle = function(cmd)
         ['Flick'] = client.random_int(-60, 60) + (globals.tickcount() % 4 == 0 and client.random_int(-180, 180) or 0), -- Лютый анхитабл
         ['Switch'] = switch_state * 90, -- Медленный рандомный свитч
 		['Spin180'] = math.sin(globals.curtime() * 5) * 180,  -- Спин на 180 градусов
-		['Jitter90'] = (globals.tickcount() % 4 == 0) and (client.random_int(0, 1) == 0 and -90 or 90)  -- Джиттер с задержкой 2 тика
+		['Jitter90'] = (globals.tickcount() % 4 == 0) and (client.random_int(0, 1) == 0 and -90 or 90),  -- Джиттер с задержкой 2 тика
 		['RandomYaw'] = client.random_int(-180, 180),  -- Рандомный угол Yaw от -180 до 180 градусов
 		['RandomStaticYaw'] = (globals.tickcount() % 2 == 0) and client.random_int(-180, 180) or 0,  -- Рандомный статичный Yaw с задержкой (каждые 2 тика)
         ['Custom'] = new_config.yaw_slider
